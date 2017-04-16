3ba062a7d6e429446f2cd65551438931373f0a26
Monina Carandang <reactor04@gmail.com>
2017-04-16 23:34:03 +0800

    Main logic is finished

--- parser.js
+++ parser.js
@@ -1,6 +1,8 @@
 var c2j = require('csvtojson')
 var processor = require('./lib/process')
 
+var files = ['ICALABAR18']
+
 var file = 'data/sample.csv'
 
 var readFromFile = function(file) {
@@ -22,5 +24,37 @@ var readFromFile = function(file) {
     })
 }
 
-readFromFile(file)
+var readFiles = function(files) {
+
+  files.forEach(function(element,  index) {
+    var data = []
+
+  c2j().fromFile('data/' + element + '.csv')
+    .on('json', function(jsonObj) {
+      data.push(jsonObj)
+    })
+    .on('done', function(err) {
+      if (err) {
+        console.log(err)
+      }
+      else {
+        var output = processor(element, data)
+
+        var fs = require('node-fs')
+
+        fs.writeFile('data/' + element + '.txt', output, function(err) {
+          if (err) {
+            console.log(err)
+          } else {
+            console.log('Finished ' + element + '.txt')
+          }
+        })
+      }
+      console.log('Done')
+    })
+  })
+}
+
+// readFromFile(file)
+readFiles(files)
 
